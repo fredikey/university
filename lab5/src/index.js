51b25c54b6243b31c576269388c7edd9a64c2d9a
Viktor Kozodaev <viktor7778123@gmail.com>
2019-10-09 00:22:46 +0300

    add sidebar with filter

--- lab5/src/index.js
+++ lab5/src/index.js
@@ -1,3 +1,18 @@
 import './sass/index.sass'
 import './img/skirt.png'
 import './img/girl.png'
+
+const elm = document.querySelector('#range');
+console.log(elm);
+const container = elm.parentNode;
+const values = elm.getAttribute('data-values').split(' ');
+
+values.forEach(function (value, i, values) {
+  const rangePart = elm.cloneNode();
+  rangePart.type = 'range';
+  rangePart.removeAttribute('data-values');
+  rangePart.value = value;
+  rangePart = container.insertBefore(rangePart, elm);
+});
+
+elm.remove();
