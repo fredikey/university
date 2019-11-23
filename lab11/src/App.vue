d20074111a52e87dc2cda2a83ef1593172bfe1d1
Victor Kozodaev <viktor7778123@gmail.com>
2019-11-24 01:05:42 +0300

    feat: add ListItem component and build dynamically rendering list in App.vue

--- lab11/src/App.vue
+++ lab11/src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form @submit="addUser" />
+    <div class="list">
+      <h2>Список тех, кого отметили</h2>
+      <div v-if="this.list.length !== 0" class="users">
+        <ListItem
+          v-for="(user, index) in this.list"
+          :key="index"
+          :user="user"
+        />
+      </div>
+      <p v-else class="empty">Тут пока пусто</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Form, ListItem } from "./components";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: () => {
+    return {
+      list: []
+    };
+  },
+  methods: {
+    addUser(user) {
+      this.list.push(user);
+    }
+  },
   components: {
-    HelloWorld
+    Form,
+    ListItem
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  background-color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  height: 100vh;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  height: 100%;
+  margin-top: 100px;
+}
+.list {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  color: #fff;
+}
+.empty {
+  font-size: 16px;
+}
+.users {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
---
+++ lab11/src/components/ListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item">
+    <span class="name">{{ this.user.name }}</span>
+    <span class="surname">{{ this.user.surname }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.name {
+  margin-right: 5px;
+}
+</style>
