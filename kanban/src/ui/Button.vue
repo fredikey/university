cd148f08ef832b83d6f1a62c4062bc5df3746136
Victor Kozodaev <viktor7778123@gmail.com>
2020-05-25 00:19:50 +0300

    add: Button UI Component

---
+++ kanban/src/ui/Button.vue
@@ -0,0 +1,30 @@
+<template>
+	<button
+		class="button"
+		v-bind="$attrs"
+		v-on="this.$listeners"
+	>
+		{{ title }}
+	</button>
+</template>
+
+<script lang="ts">
+	import Vue from "vue";
+	
+	export default Vue.extend({
+		name: "Input",
+		inheritAttrs: false,
+		props: {
+			title: String,
+		}
+	});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+	.button {
+		border: 1px solid $color-default;
+		padding: 5px 10px;
+		font-size: 16px;
+	}
+</style>
