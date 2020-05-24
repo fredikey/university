cc70cac59024a016cce56f6bd900f9141a8876c7
Victor Kozodaev <viktor7778123@gmail.com>
2020-05-25 00:20:44 +0300

    feat: make Input and Button UI components Global

--- kanban/src/main.ts
+++ kanban/src/main.ts
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import {Button, Input} from './ui'
 
 Vue.config.productionTip = false;
-
+Vue.component(
+	'ui-button',
+	Button
+)
+Vue.component(
+	'ui-input',
+	Input
+)
 new Vue({
   store,
   render: h => h(App)
