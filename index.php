29b8e1a1b45713f6a8b69387e544f36a211eb63e
Victor Kozodaev <viktor7778123@gmail.com>
2020-06-24 22:07:39 +0300

    feat: auth (sign up, sign in, logout)

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/config/database.php';
+
+use App\UsersController;
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--	Add bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--	Custom styles -->
+    <link rel="stylesheet" href="./styles/main.css">
+    <title>Exam</title>
+</head>
+<body>
+<!-- Header with Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+		<span class="logo">
+			Лого
+		</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Главная</a>
+                </li>
+                <?php if (UsersController::isLogin()) { ?>
+                    <li class="nav-login">
+                        <span class="nav-login-label">Привет, <b><?= $_SESSION['logged_user']->login; ?></b></span>
+                        <a href="logout.php" class="btn nav-logout-btn">Выход</a>
+                    </li>
+                <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signup.php">Регистрация</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Войти</a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- content -->
+<main class="main container">
+    <h1 class="text-success">
+        Привет, <?php echo (UsersController::isLogin()) ? $_SESSION['logged_user']->login : 'Гость'; ?></h1>
+    <section>
+        content1
+    </section>
+    <section>
+        content2
+    </section>
+    <section>
+        content3
+    </section>
+</main>
+<footer class="container-fluid footer">
+    <p class="footer-item lead">
+        Козодаев Виктор Сергеевич
+    </p>
+    <p class="footer-item lead">
+        191-321
+    </p>
+    <p class="footer-item lead">
+        25.06.2020
+    </p>
+    <p class="footer-item lead">
+        <a href="https://vk.com/fredikey">VK</a>
+    </p>
+</footer>
+<!-- bootstrap scripts-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,6 @@
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/config/database.php';
+
+use App\UsersController;
+
+UsersController::logout();
