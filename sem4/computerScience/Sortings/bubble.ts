cc345b2ac865b95c448c07d1598a67513d11a5ba
Victor Kozodaev <viktor7778123@gmail.com>
2021-04-04 18:17:20 +0300

    Refactor: Bubble sort

--- sem4/computerScience/Sortings/bubble.ts
+++ sem4/computerScience/Sortings/bubble.ts
@@ -1,26 +1,29 @@
 import { SortingFunc } from '../utils/types'
 
 // Bubble Sort
+// A sorting algorithm where the largest values bubble up to the top (to end of array)
 export const bubbleSort: SortingFunc = (inputArr) => {
 	// copy of array
 	const arr = [...inputArr]
 	// alg
-	// uses to check when should exit from cycle
-	let swapped = true
-	while (swapped) {
-		swapped = false
-		for (let i = 0; i < arr.length; i++) {
-			const leftEl = arr[i]
-			const rightEl = arr[i + 1]
-
-			// swap elements and mark swapped as true to launch a new cycle iteration
-			if (leftEl > rightEl) {
-				arr[i + 1] = leftEl
-				arr[i] = rightEl
-
-				swapped = true
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				// swap left and right elements
+				const tmp = arr[j]
+				arr[j] = arr[j + 1]
+				arr[j + 1] = tmp
 			}
 		}
 	}
 	return arr
 }
+// Example [10,4,3]:
+// i=3; j=0; 0 < 2; [4,10,3]
+// i=3; j=1; 1 < 2; [4,3,10]
+// i=3; j=2; 2 < 2; break
+// i=2; j=0; 0 < 1; [3,4,10];
+// i=2; j=1; 1 < 1; break
+// i=1; j=0; 0 < 0; break
+// i=0 break
+// done
