6605bdf02114d66f8f42470c8b1aeed31001c408
Victor Kozodaev <viktor7778123@gmail.com>
2021-04-04 18:18:29 +0300

    Refactor: Insert & Select sortings

--- sem4/computerScience/Sortings/select.ts
+++ sem4/computerScience/Sortings/select.ts
@@ -1,23 +1,26 @@
 import { SortingFunc } from '../utils/types'
 
 // Select Sort
+// The selection sort algorithm sorts an array by repeatedly
+// finding the minimum element (considering ascending order)
+// from unsorted part and putting it at the beginning.
 export const selectSort: SortingFunc = (inputArr) => {
 	// copy of array
 	const arr = [...inputArr]
 	// alg
 	for (let i = 0; i < arr.length; i++) {
 		// uses to store index of minimal value
-		let idx = i
+		let minimalIdx = i
 		for (let j = i; j < arr.length; j++) {
 			// find index of minimal value
-			if (arr[j] < arr[idx]) {
-				idx = j
+			if (arr[j] < arr[minimalIdx]) {
+				minimalIdx = j
 			}
 		}
 		// swap elements
-		if (idx !== i) {
-			const tmp = arr[idx]
-			arr[idx] = arr[i]
+		if (minimalIdx !== i) {
+			const tmp = arr[minimalIdx]
+			arr[minimalIdx] = arr[i]
 			arr[i] = tmp
 		}
 	}
